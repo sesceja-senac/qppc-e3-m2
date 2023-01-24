732adf2f8769be22c913e81a1e933643a0761925
brunaflores-gete <80692164+brunaflores-gete@users.noreply.github.com>
2023-01-24 16:56:32 -0300

    página 2 finalizada

---
+++ conteudo/js/custom.js
@@ -0,0 +1,17 @@
+$(function () {
+  if (window.location.search == '?page=1') {
+
+      $('footer').addClass('footer-inicial');
+
+      var pg1ImgAltura = $('.abaixo').height();
+      var pg1LarguraContainer = $('.pagina1 .container').width();
+
+      $('.footer-inicial').css({
+          'bottom': pg1ImgAltura - 100,
+          'right': pg1LarguraContainer - 300
+      });
+  }
+
+  if (window.location.search == '?page=2') { $('footer').addClass('footer-pg2'); }
+
+});
